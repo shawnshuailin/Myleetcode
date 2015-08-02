26a5135ef59f4d2448cd46126a229294abe67a05
林帅 <shawn.shuai.lin@gmail.com>
2015-08-02 09:26:52 +0100

    new solved problem rotate_list, but not the best in running time, need further modification.

---
+++ rotate_list.cpp
@@ -0,0 +1,26 @@
+ListNode* rotateRight(ListNode* head, int k) 
+{
+    if (k <= 0 || head == NULL)
+    return head;
+    ListNode* newHead = head;
+    ListNode* end = head;
+    int iTotalNode = 1;
+    while (end->next != NULL)
+    {
+        end = end->next;
+        ++iTotalNode;
+    }
+    k = k % iTotalNode;
+    if (k <= 0)
+    return head;
+    iTotalNode -= k;
+    while (iTotalNode > 0)
+    {
+        end->next = newHead;
+        end = newHead;
+        newHead = newHead->next;
+        end->next = NULL;
+        --iTotalNode;
+    }
+    return newHead;
+}
